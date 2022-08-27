ab4cbdb07c9e0211ec7ccc0be17143d5ad23a187
Haik <55463651+salampego@users.noreply.github.com>
2022-08-27 20:01:38 +0300

    update files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { SearchBar } from './SearchBar/SearchBar';
+
+import './App.css';
+
+export class App extends Component {
+  render() {
+    return <SearchBar />;
+  }
+}
