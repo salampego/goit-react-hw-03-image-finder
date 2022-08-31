6c1fe18c22fce06d316b0ac6faf37b8b269b6a1d
Haik <55463651+salampego@users.noreply.github.com>
2022-08-31 17:50:02 +0300

    correct the error

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,6 +1,5 @@
 import s from './ImageGallery.module.css';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Button } from 'components/Button/Button';
 export const ImageGallery = ({ imageGallery }) => {
   return (
     <ul className={s.ImageGallery}>
